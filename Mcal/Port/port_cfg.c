8a700499b0c6bdb61922d2bcaee917d8b97192a6
creyes <reyescristopher43@gmail.com>
2026-08-07 11:41:52 -0600

    fix(Mcal/Port): correct typo in extern declaration of Port_Config array

--- Mcal/Port/port_cfg.c
+++ Mcal/Port/port_cfg.c
@@ -1,14 +1,14 @@
 #include "port_cfg.h"
 
-const Port_ConfigiDataType Port_Config[] = {
+const Port_ConfigDataType Port_Config[] = {
 	{
 		.PinId = PORT_A_PIN_5,
-		.Pin_Mode = PORT_PIN_MODE_DIO,
-		.Pin_Direction PORT_PIN_OUT,
+		.Pin_Mode = PORT_PIN_MODE_DO,
+		.Pin_Direction = PORT_PIN_OUT,
 		.Pin_Speed = PORT_PIN_MEDIUM_SPEED,
 		.Pin_OutputType = PORT_PIN_OUT_OPEN_DRAIN,
 		.Pin_ResistorType = PORT_PIN_RESISTOR_DISABLE,
-		.DirectionChangable = ,
-		.ModeChangable = 
+		.DirectionChangable = TRUE,
+		.ModeChangable = TRUE
 	}
-}
+};
