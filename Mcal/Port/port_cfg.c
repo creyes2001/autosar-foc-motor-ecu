03032865a48205c0912aa804881a915f3799d811
creyes <reyescristopher43@gmail.com>
2026-08-10 12:45:24 -0600

    feat(Mcal/Port): add MCO pin configuration

--- Mcal/Port/port_cfg.c
+++ Mcal/Port/port_cfg.c
@@ -2,6 +2,7 @@
 
 const Port_ConfigDataType Port_CfgData[] = {
 	{
+		//On-board led configuration
 		.PinId = PORT_A_PIN_5,
 		.Pin_Mode = PORT_PIN_MODE_DO,
 		.Pin_Direction = PORT_PIN_OUT,
@@ -10,6 +11,17 @@ const Port_ConfigDataType Port_CfgData[] = {
 		.Pin_ResistorType = PORT_PIN_RESISTOR_PULLUP,
 		.DirectionChangable = TRUE,
 		.ModeChangable = TRUE
+	},
+	{
+		//MCO pin configuration 
+		.PinId = PORT_A_PIN_8,
+		.Pin_Mode = PORT_PIN_MODE_AF0,
+		.Pin_Direction = PORT_PIN_OUT,
+		.Pin_Speed = PORT_PIN_HIGH_SPEED,
+		.Pin_OutputType = PORT_PIN_OUT_PUSH_PULL,
+		.Pin_ResistorType = PORT_PIN_RESISTOR_DISABLE,
+		.DirectionChangable = FALSE,
+		.ModeChangable = FALSE
 	}
 };
 
