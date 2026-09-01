fbf802ed217e4a59e2e5a09a38f7fc2a792adb1d
creyes <reyescristopher43@gmail.com>
2026-08-31 22:04:24 -0600

    feat(Mcal/Adc): add Adc module fucntion prototypes

---
+++ Mcal/Adc/adc.h
@@ -0,0 +1,16 @@
+#ifndef ADC_H
+#define ADC_H
+
+void Adc_Init (const Adc_ConfigType* ConfigPtr);
+Std_ReturnType Adc_SetupResultBuffer (Adc_GroupType Group,Adc_ValueGroupType* DataBufferPtr);
+void Adc_DeInit (void);
+void Adc_StartGroupConversion (Adc_GroupType Group);
+void Adc_StopGroupConversion (Adc_GroupType Group);
+Std_ReturnType Adc_ReadGroup (Adc_GroupType Group,Adc_ValueGroupType* DataBufferPtr);
+void Adc_EnableHardwareTrigger (Adc_GroupType Group);
+void Adc_DisableHardwareTrigger (Adc_GroupType Group);
+Adc_StatusType Adc_GetGroupStatus (Adc_GroupType Group);
+Adc_StreamNumSampleType Adc_GetStreamLastPointer (Adc_GroupType Group,Adc_ValueGroupType** PtrToSamplePtr);
+
+
+#endif //ADC_H
