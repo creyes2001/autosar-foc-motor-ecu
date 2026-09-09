93a40cd82f56d36557ad0762e906967cce9dbb03
creyes <reyescristopher43@gmail.com>
2026-09-08 18:20:17 -0600

    fix(Mcal): add missing includes and correct us type declaration inside Mcal_DelayUs function

--- Mcal/mcal.h
+++ Mcal/mcal.h
@@ -2,8 +2,10 @@
 #define MCAL_H
 
 #include "Std_Types.h"
+#include "Platform_Types.h"
+#include "stm32g431xx.h"
 
-void Mcal_DelayUs(uint32_t us);
+void Mcal_DelayUs(uint32 us);
 Std_ReturnType Mcal_WaitBitTimeout(volatile uint32 *Reg, uint32 Mask, uint32 ExpectedValue, uint32 TimeoutUs);
 
 #endif //MCAL_H
