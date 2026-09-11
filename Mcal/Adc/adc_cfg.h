9c119f820295a8c17f713b0143175b9b045e31bf
creyes <reyescristopher43@gmail.com>
2026-09-10 18:23:01 -0600

    fix(Mcal/Adc): correct declaration definitions

--- Mcal/Adc/adc_cfg.h
+++ Mcal/Adc/adc_cfg.h
@@ -4,8 +4,8 @@
 #include "Std_Types.h"
 #include "Platform_Types.h"
 
-typedef uint Adc_ChannelType;	//ID of an ADC channel
-typedef uint Adc_GroupType;		//ID of an ADC group
+typedef uint8 Adc_ChannelType;	//ID of an ADC channel
+typedef uint8 Adc_GroupType;		//ID of an ADC group
 typedef uint8 Adc_HWUnitType;
 
 #define ADC1_CH1 ((Adc_ChannelType)0x00)
@@ -64,110 +64,110 @@ typedef uint8 Adc_HWUnitType;
 #define ADC_640_5_CLOCK_CYCLES	((Adc_SamplingTimeType)0x07)
 
 typedef int Adc_ValueGroupType;
-typedef uint Adc_PrescaleType;
-typedef uint Adc_ConversionTimeType;
+typedef uint8 Adc_PrescaleType;
+typedef uint8 Adc_ConversionTimeType;
 typedef uint8 Adc_SamplingTimeType;
 typedef uint8 Adc_ResolutionType;
-typedef Adc_GroupDefType; //assigment of channels to a chanel group
-typedef uint Adc_StreamNumSampleType;
-typedef uint8 Adc_NumerOfConversionsType;
+typedef uint8 Adc_GroupDefType; //assigment of channels to a chanel group
+typedef uint8 Adc_StreamNumSampleType;
+typedef uint8 Adc_NumberOfConversionsType;
 typedef uint8 Adc_NumberOfGroupsType;
 
 typedef enum{
-	ADC_INPUT_SINGLE_ENDED = 0x00;
-	ADC_INPUT_DIFFERENTIAL = 0x01;
+	ADC_INPUT_SINGLE_ENDED = 0x00,
+	ADC_INPUT_DIFFERENTIAL = 0x01
 }Adc_InputModeType;
 
 typedef enum{
-	ADC_REGULAR_CONVERSION = 0x00;
-	ADC_INJECTED_CONVERSION = 0x01;
+	ADC_REGULAR_CONVERSION = 0x00,
+	ADC_INJECTED_CONVERSION = 0x01
 }Adc_ConversionType;
 
 typedef enum{
-	ADC_STREAM_BUFFER_LINEAR = 0x00;
-	ADC_STREAM_BUFFER_CIRCULAR = 0x01;
+	ADC_STREAM_BUFFER_LINEAR = 0x00,
+	ADC_STREAM_BUFFER_CIRCULAR = 0x01
 }Adc_StreamBufferModeType;
 
 typedef enum{
-	ADC_ACCESS_MODE_SINGLE = 0x00;
-	ADC_ACCESS_MODE_STREAMING = 0x01;
+	ADC_ACCESS_MODE_SINGLE = 0x00,
+	ADC_ACCESS_MODE_STREAMING = 0x01
 }Adc_GroupAccessModeType;
 
 typedef enum{
-	ADC_HW_TRIG_RISING_EDGE = 0x00;
-	ADC_HW_TRIG_FALLING_EDGE = 0x01;
-	ADC_HW_TRIG_BOTH_EDGES = 0x02;
+	ADC_HW_TRIG_RISING_EDGE = 0x00,
+	ADC_HW_TRIG_FALLING_EDGE = 0x01,
+	ADC_HW_TRIG_BOTH_EDGES = 0x02
 }Adc_HwTriggerSignalType;
 
 typedef enum{
-	ADC_IDLE = 0x00;
-	ADC_BUSY = 0x01;
-	ADC_COMPLETED = 0x02;
-	ADC_STREAM_COMPLETED = 0x03;
+	ADC_IDLE = 0x00,
+	ADC_BUSY = 0x01,
+	ADC_COMPLETED = 0x02,
+	ADC_STREAM_COMPLETED = 0x03
 }Adc_StatusType;
 
 typedef enum{
-	ADC_TRIGG_SRC_SW = 0x00;
-	ADC_TRIGG_SRC_HW = 0x01;
+	ADC_TRIGG_SRC_SW = 0x00,
+	ADC_TRIGG_SRC_HW = 0x01
 }Adc_TriggerSourceType;
 
 typedef enum{
-	ADC_CONV_MODE_ONESHOT = 0x00;
-	ADC_CONV_MODE_CONTINUOUS = 0x01;
+	ADC_CONV_MODE_ONESHOT = 0x00,
+	ADC_CONV_MODE_CONTINUOUS = 0x01
 }Adc_GroupConvModeType;
 
 typedef enum{
-	ADC_RANGE_UNDER_LOW = 0x00;
-	ADC_RANGE_BETWEEN = 0x01;
-	ADC_RANGE_OVER_HIGH = 0x02;
-	ADC_RANGE_ALWAYS = 0x03;
-	ADC_RANGE_NOT_UNDER_LOW = 0x04;
-	ADC_RANGE_NOT_BETWEEN = 0x05;
-	ADC_RANGE_NOT_OVER_HIGH = 0x06;
+	ADC_RANGE_UNDER_LOW = 0x00,
+	ADC_RANGE_BETWEEN = 0x01,
+	ADC_RANGE_OVER_HIGH = 0x02,
+	ADC_RANGE_ALWAYS = 0x03,
+	ADC_RANGE_NOT_UNDER_LOW = 0x04,
+	ADC_RANGE_NOT_BETWEEN = 0x05,
+	ADC_RANGE_NOT_OVER_HIGH = 0x06
 }Adc_ChannelRangeSelectType;
 
 typedef enum{
-	ADC_ALIGN_LEFT = 0x00;
-	ADC_ALIGN_RIGHT = 0x01;
-}Adc_ResultAlignmentType;
+	ADC_ALIGN_LEFT = 0x00,
+	ADC_ALIGN_RIGHT = 0x01
+}Adc_ResultAligmentType;
 
 //struct for channel specific configuration data
 typedef struct{
 	Adc_ChannelType Channel;
 	Adc_InputModeType InputMode;
 	Adc_SamplingTimeType SamplingTime;
-	Adc_ChannelRangeSelecttype ChannelRange;
+	Adc_ChannelRangeSelectType ChannelRange;
 }Adc_ChannelConfigType;
 
 typedef struct{
 	Adc_GroupType GroupType; 	
-	Adc_NumberOfConvertionsType NumberOfConversions; //Channels to be converted
+	Adc_NumberOfConversionsType NumberOfConversions; //Channels to be converted
 	Adc_ConversionType ConversionType; //injected or regular
 	Adc_ValueGroupType ValueGroup;
-	Adc_GroupConvModetype ConversionMode;
-	Adc_ConversionTimetype ConversionTimeType;
+	Adc_GroupConvModeType ConversionMode;
+	Adc_ConversionTimeType ConversionTimeType;
 	Adc_TriggerSourceType TriggerSource;
 	Adc_HwTriggerSignalType HwTriggerSignal;
 	Adc_GroupAccessModeType GroupAccessMode;
-	Adc_Stream_BufferModeType StreamBufferMode;
-	Adc_Stream_NumSampleType StreamNumSample;
-	const Adc_ChannelConfigtype* Adc_ChannelConfig;
+	Adc_StreamBufferModeType StreamBufferMode;
+	Adc_StreamNumSampleType StreamNumSample;
+	const Adc_ChannelConfigType* Adc_ChannelConfig;
 }Adc_GroupConfigType;
 
 typedef struct{
 	Adc_HWUnitType Adc_HWUnit;
 	Adc_ResolutionType Adc_Resolution;
 	Adc_ResultAligmentType ResultAligment;
-	const Adc_GroupConfigType* Adc_GroupCofig;
+	const Adc_GroupConfigType* Adc_GroupConfig;
 	Adc_NumberOfGroupsType Adc_NumberOfGroups;
 }Adc_ConfigDataType;
 
 //struct for ADC configuration data
 typedef struct{
-	Adc_ConfigDataType* Adc_ConfigData; 
+	const Adc_ConfigDataType* Adc_ConfigData; 
 	uint8 size;
 }Adc_ConfigType;
 
-extern const Adc_Configtype* Adc_Config;
+extern const Adc_ConfigType* Adc_Config;
 
 #endif //ADC_CFG_H
