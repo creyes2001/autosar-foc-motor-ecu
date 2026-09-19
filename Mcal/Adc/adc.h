4b9881920df279614104a01ff029c8ab239cb28d
creyes <reyescristopher43@gmail.com>
2026-09-18 18:06:58 -0600

    fix(Mcal/Adc): remove unused function prototypes

--- Mcal/Adc/adc.h
+++ Mcal/Adc/adc.h
@@ -5,7 +5,6 @@
 #include "stm32g431xx.h"
 
 void Adc_Init (const Adc_ConfigType* ConfigPtr);
-Std_ReturnType Adc_SetupResultBuffer (Adc_GroupType Group,Adc_ValueGroupType* DataBufferPtr);
 void Adc_DeInit (void);
 void Adc_StartGroupConversion (Adc_GroupType Group);
 void Adc_StopGroupConversion (Adc_GroupType Group);
@@ -13,7 +12,5 @@ Std_ReturnType Adc_ReadGroup (Adc_GroupType Group,Adc_ValueGroupType* DataBuffer
 void Adc_EnableHardwareTrigger (Adc_GroupType Group);
 void Adc_DisableHardwareTrigger (Adc_GroupType Group);
 Adc_StatusType Adc_GetGroupStatus (Adc_GroupType Group);
-Adc_StreamNumSampleType Adc_GetStreamLastPointer (Adc_GroupType Group,Adc_ValueGroupType** PtrToSamplePtr);
-
 
 #endif //ADC_H
